f27e99e6d388db9fb0683f3c8ef9c93f90b53279
Artem <artemdem972@gmail.com>
2022-05-23 17:47:35 +0300

    Some changes

--- UP_Exercise 3/MainWindow.xaml.cs
+++ UP_Exercise 3/MainWindow.xaml.cs
@@ -73,7 +73,80 @@ namespace UP_Exercise_3
         {
             join_result.Text = string.Join(join_second_string.Text, join_first_string.Text.Split());
         }
-
+        private void Replace_Click(object sender, RoutedEventArgs e)
+        {
+            replace_result.Text = replace_string.Text.Replace(replace_old_string.Text, replace_new_string.Text);
+        }
+        private void Split_Click(object sender, RoutedEventArgs e)
+        {
+            split_result.Text = "";
+            string[] split_res = split_first_string.Text.Split(split_second_string.Text);
+            foreach (string elem in split_res)
+            {
+                split_result.Text += elem + "\n";
+            }
+        }
+        private void Substring_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (substring_length.Text == "")
+                {
+                    Exception ex = ExceptionFunctions.Ex_Int(substring_start.Text, "\"Начальный индекс\"", 0);
+                    if (ex == null)
+                    {
+                        substring_result.Text = substring_string.Text.Substring(Convert.ToInt32(substring_start.Text));
+                    }
+                    else
+                    {
+                        throw ex;
+                    }
+                }
+                else
+                {
+                    Exception[] ex = new Exception[2];
+                    ex[0] = ExceptionFunctions.Ex_Int(substring_start.Text, "\"Начальный индекс\"", 0);
+                    ex[1] = ExceptionFunctions.Ex_Int(substring_length.Text, "\"Длина подстроки\"", 0);
+                    if(ex[0] == null && ex[1] == null)
+                    {
+                        substring_result.Text = substring_string.Text.Substring(Convert.ToInt32(substring_start.Text), Convert.ToInt32(substring_length.Text));
+                    }
+                    else
+                    {
+                        string error = "";
+                        foreach(Exception exep in ex)
+                        {
+                            if(exep != null)
+                            {
+                                error += exep.Message + "\n";
+                            }
+                        }
+                        throw new Exception(error);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            
+        }
+        private void ToUL_Click(object sender, RoutedEventArgs e)
+        {
+            toul_result.Text = $"ToUpper: {toul_string.Text.ToUpper()} \nToLower: {toul_string.Text.ToLower()}";
+        }
+        private void Trim_Click(object sender, RoutedEventArgs e)
+        {
+            if (trim_char.Text == "")
+            {
+                trim_result.Text = trim_string.Text.Trim();
+            }
+            else
+            {
+                char[] chr = trim_char.Text.ToCharArray();
+                trim_result.Text = trim_string.Text.Trim(chr);
+            }
+        }
         private void Main_Exit_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.MainWindow.Close();
